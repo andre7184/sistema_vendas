8d7c47034e69291bd361ed0f2b01a222033836cb
André Martins Brandão <andre53793016@aluno.ms.senac.br>
2024-12-04 21:43:24 -0400

    atualizado

--- api/db/connection.py
+++ api/db/connection.py
@@ -3,7 +3,7 @@ import mysql.connector
 def get_connection():
     return mysql.connector.connect(
         host="localhost",
-        user="root",
-        password="",
+        user="andre",
+        password="Amb@8484",
         database="sistema_vendas"
     )
